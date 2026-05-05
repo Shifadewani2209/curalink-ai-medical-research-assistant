017e99956ea19ebc14566bfd94316f8d012edbf3
Shifadewani2209 <dewanishifa2003@gmail.com>
2026-05-05 12:59:53 +0530

    fix cors setup

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,14 @@
+
 import express from "express";
 import cors from "cors";
 import researchRoutes from "./routes/researchRoutes.js";
 import patientRoutes from "./routes/patientRoutes.js";
 
 const app = express();
+app.use(cors({
+  origin: "*"
+}));
 
-app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
