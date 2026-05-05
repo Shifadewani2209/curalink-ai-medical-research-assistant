2a7b7b1579defe1d180e008068b0abbd18b60e51
Shifadewani2209 <dewanishifa2003@gmail.com>
2026-05-05 12:32:20 +0530

    convert backend to vercel serverless

--- server/src/index.js
+++ server/src/index.js
@@ -9,9 +9,8 @@ const PORT = process.env.PORT || 5000;
 const startServer = async () => {
   await connectDB();
 
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+  
 };
+export default app;
 
 startServer();
